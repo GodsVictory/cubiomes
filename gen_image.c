7db16f21c7b8ffa27a0da4275e146f1efe5a635e
Thomas Levy <tlevy@evernote.com>
2020-05-02 00:56:51 -0700

    fix image_gen free bug by not freeing

--- gen_image.c
+++ gen_image.c
@@ -2,37 +2,37 @@
 #include "util.h"
 #include <emscripten.h>
 
-unsigned char* EMSCRIPTEN_KEEPALIVE gen_image(uint32_t lo, uint32_t hi) {
-  int64_t s = (uint64_t)lo | ((uint64_t)hi << 32);
-  unsigned char biomeColours[256][3];
-  initBiomes();
+unsigned char rgb[4*512*512];
+int *biomeCache;
+LayerStack g;
+unsigned char biomeColours[256][3];
+unsigned int areaWidth = 256, areaHeight = 256;
 
+
+unsigned char* EMSCRIPTEN_KEEPALIVE get_buffer() {
+  initBiomes();
+	g = setupGenerator(MC_1_15);
+  // Allocate a sufficient buffer for the biomes and for the image pixels.
+  biomeCache = allocCache(&g.layers[L_SHORE_16], areaWidth, areaHeight);
   // Initialize a colour map for biomes.
   initBiomeColours(biomeColours);
+  return rgb;
+}
+
+void EMSCRIPTEN_KEEPALIVE gen_image(uint32_t lo, uint32_t hi) {
+  int64_t s = (uint64_t)lo | ((uint64_t)hi << 32);
 
   // Extract the desired layer.
-	LayerStack g = setupGenerator(MC_1_15);
   Layer *layer = &g.layers[L_SHORE_16];
 
   int areaX = -128, areaZ = -128;
-  unsigned int areaWidth = 256, areaHeight = 256;
   unsigned int scale = 2;
   unsigned int imgWidth = areaWidth * scale, imgHeight = areaHeight * scale;
 
-  // Allocate a sufficient buffer for the biomes and for the image pixels.
-  int *biomes = allocCache(layer, areaWidth, areaHeight);
-  unsigned char *rgb = (unsigned char *)malloc(4 * imgWidth * imgHeight);
-
   // Apply the seed only for the required layers and generate the area.
   setWorldSeed(layer, s);
-  genArea(layer, biomes, areaX, areaZ, areaWidth, areaHeight);
+  genArea(layer, biomeCache, areaX, areaZ, areaWidth, areaHeight);
 
   // Map the biomes to a color buffer and save to an image.
-  biomesToImage(rgb, biomeColours, biomes, areaWidth, areaHeight, scale, 2, 1);
-
-  // Clean up.
-  free(biomes);
-  freeGenerator(g);
-
-  return rgb;
+  biomesToImage(rgb, biomeColours, biomeCache, areaWidth, areaHeight, scale, 2, 1);
 }
